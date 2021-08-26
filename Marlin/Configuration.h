398631904ec888097c2e3185435dad5f1b128b8c
Jonathan Liu <jonathan.liu@mosaicmfg.com>
2021-08-26 15:00:53 -0400

    1. Disable Z_SAFE_HOMING as it prevents running G28 Z anywhere except the middle of the bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1635,7 +1635,7 @@
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing.
 // - Prevent Z homing when the Z probe is outside bed area.
 //
-#define Z_SAFE_HOMING
+//#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
 #define Z_SAFE_HOMING_X_POINT ((X_BED_SIZE) / 2) // X point for Z homing
