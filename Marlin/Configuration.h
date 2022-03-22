4621caf42dfd5120d19aff7ef66b5730f900bc7d
ScotttG <scott@mosaicmanufacturing.com>
2022-03-22 15:19:50 -0400

    homing setup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -462,9 +462,9 @@
 //#define TEMP_SENSOR_1_AS_REDUNDANT
 #define MAX_REDUNDANT_TEMP_SENSOR_DIFF 10
 
-#define TEMP_RESIDENCY_TIME 10 // (seconds) Time to wait for hotend to "settle" in M109
+#define TEMP_RESIDENCY_TIME 3 // (seconds) Time to wait for hotend to "settle" in M109
 #define TEMP_WINDOW 1          // (°C) Temperature proximity for the "temperature reached" timer
-#define TEMP_HYSTERESIS 3      // (°C) Temperature proximity considered "close enough" to the target
+#define TEMP_HYSTERESIS 15      // (°C) Temperature proximity considered "close enough" to the target
 
 #define TEMP_BED_RESIDENCY_TIME 10 // (seconds) Time to wait for bed to "settle" in M190
 #define TEMP_BED_WINDOW 1          // (°C) Temperature proximity for the "temperature reached" timer
