58c66e88ceb09bf29d5ed433a300f39f94f2bd58
ScotttG <scott@mosaicmanufacturing.com>
2023-05-18 16:24:00 -0400

    Aqua-NO-THERMAL-PROTECTION

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -239,10 +239,10 @@
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
 #define THERMAL_PROTECTION_PERIOD 180    // Seconds
-#define THERMAL_PROTECTION_HYSTERESIS 100 // Degrees Celsius
+#define THERMAL_PROTECTION_HYSTERESIS 300 // Degrees Celsius
 
 //#define ADAPTIVE_FAN_SLOWING              // Slow part cooling fan if temperature drops
-#if BOTH(ADAPTIVE_FAN_SLOWING, PIDTEMP)
+#if BOTH(ADAPTIVE_FAN_SLOWING, PIDTEMP)WATCH_TEMP
 //#define NO_FAN_SLOWING_IN_PID_TUNING    // Don't slow fan speed during M303
 #endif
 
